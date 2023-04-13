d9cbd1e3ed8a050169c26ff8c388f14c8ce05947
tamasadanko <tamas.adanko@guidesmiths.com>
2023-04-13 09:08:02 +0200

    feat: missing words warning

--- src/Test/index.tsx
+++ src/Test/index.tsx
@@ -28,40 +28,46 @@ function Test({ showMenu }: { showMenu: () => void }) {
   };
 
   return <div>
-    <div className='test-word'>
-      {
-        correct === null ?
-          <div> {word.hungarian}</div>
-          : correct ?
-            <div style={{color: 'green'}}>{word.foreign}</div> :
-            <div style={{color: 'red'}}>{word.foreign}</div>
-      }
-    </div>
-    <div>
-      <input
-        type='text'
-        onChange={(e) => setInput(e.target.value)}
-        placeholder={'megfejtés'}
-        value={input}
-        onKeyDown={handleKeyDown}
-        disabled={showCorrect}
-      ></input>
-    </div>
-    {showCorrect && <>
-			<div className={'buttonHold'}>
-				<button
-					className={`btn`}
-					onClick={onNext}>Következő
-				</button>
-			</div>
-		</>}
-    {!showCorrect &&
-			<div className={'buttonHold'}>
-				<button
-					className={`btn`}
-					onClick={check}>Ellenőrzés
-				</button>
-			</div>
+    {dictionary.length ? <>
+        <div className='test-word'>
+          {
+            correct === null ?
+              <div> {word.hungarian}</div>
+              : correct ?
+                <div style={{ color: 'green' }}>{word.foreign}</div> :
+                <div style={{ color: 'red' }}>{word.foreign}</div>
+          }
+        </div>
+        <div>
+          <input
+            type='text'
+            onChange={(e) => setInput(e.target.value)}
+            placeholder={'megfejtés'}
+            value={input}
+            onKeyDown={handleKeyDown}
+            disabled={showCorrect}
+          ></input>
+        </div>
+        {showCorrect && <>
+					<div className={'buttonHold'}>
+						<button
+							className={`btn`}
+							onClick={onNext}>Következő
+						</button>
+					</div>
+				</>}
+        {!showCorrect &&
+					<div className={'buttonHold'}>
+						<button
+							className={`btn`}
+							onClick={check}>Ellenőrzés
+						</button>
+					</div>
+        }
+      </> :
+      <div className={'test-warning'}>
+        Kérlek adj hozzá szavakat a szótárban
+      </div>
     }
     <div className={'buttonHold'}>
       <button
